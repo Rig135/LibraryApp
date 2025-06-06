f4a9ad8af4df731d8eb2feb718dadc218ad45dd8
Rig135 <167835509+Rig135@users.noreply.github.com>
2025-06-06 18:09:23 +0530

    add card content

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const Library = [];
+const addBook = document.querySelector('#addFinishedBook');
+const finishedBooks = document.querySelectorAll('.finished-cardArea > div')
+const unfinishedBooks = document.querySelectorAll('.toRead-cardArea > div')
 
 function Books(id, title, author, pages, read) {
     this.id = id;
@@ -18,4 +21,31 @@ function addBooksToLibrary(title, author, pages, read) {
 }
 
 addBooksToLibrary('babaBlack Sheep', 'mememe', 20, true);
+addBooksToLibrary('Atomic Habits', 'James Clear', 390, true);
+addBooksToLibrary('The Subtle Art of not giving a fuck', 'idkman', 223, true);
+addBooksToLibrary('Programmming Interviews', 'mark leu', 500, false);
+
+
 console.log(Library);
+
+let finIndex = 0;
+let unFinIndex = 0;
+for (let i = 0; i < Library.length; i++) {
+
+    let str = Library[i].title + "<br>";
+    str += "Author:-  " + Library[i].author + "<br>";
+    str += "Pages:- " + Library[i].pages + "<br>";
+
+    if (Library[i].read === true && finIndex < finishedBooks.length) {
+        str += "Read:- ✅";
+        finishedBooks[finIndex].innerHTML = str;
+        finIndex++;
+    }
+    else if (unFinIndex < unfinishedBooks.length) {
+        str += "Read:- ❌";
+        unfinishedBooks[unFinIndex].innerHTML = str;
+        unFinIndex++;
+    }
+
+}
+
